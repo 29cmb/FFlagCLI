7e5a25c6d37ac1a2947fe016724bb03ef0f9e88d
Cmb <71194682+29cmb@users.noreply.github.com>
2024-07-19 15:25:39 -0400

    logs and flags command

--- bin/index.js
+++ bin/index.js
@@ -1,14 +1,35 @@
 #!/usr/bin/env node
 
 const yargs = require("yargs");
+const axios = require("axios")
+
+const baseAPIURL = "https://flags.devcmb.hackclub.app"
 
 yargs.command({
     command: "flags",
     describe: "Get all flag values",
-    builder: (yargs) => {
-        return yargs
-    },
-    handler: (argv) => {
-        console.log("Good morning")
+    builder: (yargs) => { return yargs },
+    handler: async (argv) => {
+        await axios.get(`${baseAPIURL}/api/flags`).then(response => {
+            response.data.data.forEach((flag) => {
+                console.log(`${flag.locked == true ? "🔒 " : ""}${flag.flag}: ${typeof flag.value == "boolean" ? (flag.value == true ? "✅" : "❌") : flag.value}`)
+            })
+        }).catch((err) => {
+            console.log("An error occured")
+        })
     }
-}).demandCommand(1).help().argv
+}).command({
+    command: "logs",
+    describe: "View the flag logs",
+    builder: (yargs) => { return yargs },
+    handler: async (argv) => {
+        await axios.get(`${baseAPIURL}/api/logs`).then(response => {
+            response.data.data.forEach((flag) => {
+                console.log(flag.message)
+            })
+        }).catch((err) => {
+            console.log("An error occured")
+        })
+    }
+})
+.demandCommand(1).help().argv
